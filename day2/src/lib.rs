ce0d53d95c817477d3e2821d9ddb5fd6a332a28e
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2021-12-14 10:50:28 +0100

    Add day2 part 1

---
+++ day2/src/lib.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+pub fn part1() -> String {
+    let file = File::open("./day2/src/input_1.txt").expect("could not open file");
+    let reader = BufReader::new(file);
+
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let v: Vec<&str> = line.split(' ').collect();
+        match v[0] {
+            "forward" => x += v[1].parse::<usize>().unwrap(),
+            "down" => y += v[1].parse::<usize>().unwrap(),
+            "up" => y -= v[1].parse::<usize>().unwrap(),
+            _ => continue,
+        }
+    }
+    String::from(format!("{}", x * y))
+}
+
+pub fn part2() -> String {
+    String::from("part_2")
+}
--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,4 +1,5 @@
 use day1;
+use day2;
 
 fn main() {
     let d1_p1 = day1::part1();
@@ -6,4 +7,7 @@ fn main() {
 
     let d1_p2 = day1::part2();
     println!("Solution d1p2: {}", d1_p2);
+
+    let d2_p1 = day2::part1();
+    println!("Solution d2p1: {}", d2_p1);
 }
